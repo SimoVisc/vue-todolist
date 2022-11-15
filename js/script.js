ad30cfcb3e01e9faef071fe5fbd341aaaa401474
SimoVisc <simonaviscardivs@gmail.com>
2022-11-15 15:52:39 +0100

    Aggiunge l'arrey di oggetti

--- js/script.js
+++ js/script.js
@@ -3,7 +3,18 @@ const { createApp } = Vue;
   createApp({
     data() {
       return {
-        
+        todoList: [
+            {
+               text:"fare la spesa",
+               done: true
+            },{
+               text:"dare da mangiare al gatto",
+               done: true
+            },{
+               text:"pagare le bollette",
+               done: false
+            }
+        ]
       }
     }
   }).mount('#app')
