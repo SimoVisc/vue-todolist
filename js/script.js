6ce93b3d9ca8291212e9cfdfd497b1d94030fc6c
SimoVisc <simonaviscardivs@gmail.com>
2022-11-16 10:16:54 +0100

    Sistema gli errori

--- js/script.js
+++ js/script.js
@@ -6,21 +6,29 @@ const { createApp } = Vue;
         newTodo:"",
         todoList: [
             {
-               text:"fare la spesa",
-               done: true
+              text:"fare la spesa",
+              done: false
             },{
-               text:"dare da mangiare al cane",
-               done: true
+              text:"dare da mangiare al cane",
+              done: true
             },{
-               text:"pagare le bollette",
-               done: false
+              text:"pagare le bollette",
+              done: false
             }
         ]
       }
     }, 
     methods: {
         newTask(){
+          const objNewtodo ={
+            text: this.newTodo,
+            done: false
+          }
           this.todoList.push(this.newTodo);
+          this.newTodo= ""
+        },
+        removeTask(i){
+         this.todoList.splice(i,1)
         }
     }
   }).mount('#app')
